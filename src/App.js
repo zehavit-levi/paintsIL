094e68c223e527d833cb9d2f56863b47c3c777dc
Zehavit <levi.zehavit@gmail.com>
2021-05-05 14:22:57 +0300

    add routing  + cleanup + add active user context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import HomePage from './pages/HomePage/HomePage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import ShowCreatorDetailsPage from './pages/ShowCreatorDetailsPage/ShowCreatorDetailsPage';
+import WellcomePage from './pages/WellcomePage/WellcomePage';
+import CreatorManagePage from './pages/CreatorManagePage/CreatorManagePage';
+import BuyerSavedPaintsPage from './pages/BuyerSavedPaintsPage/BuyerSavedPaintsPage';
+import CreationDetailsPage from './pages/CreationDetailsPage/CreationDetailsPage';
+import CreatorNewPaintPage from './pages/CreatorNewPaintPage/CreatorNewPaintPage';
+import CreatorDetailsPage from './pages/CreatorDetailsPage/CreatorDetailsPage';
+import ActiveUserContext from './shared/ActiveUserContext';
+import { useState } from 'react';
 function App() {
+  const [activeUser, setActiveUser] = useState();
   return (
-   <div className="App">
-      
-    </div>
+    <ActiveUserContext.Provider value={activeUser} >
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" ><WellcomePage /></Route>
+          <Route exact path="/login" component={LoginPage}></Route>
+          <Route exact path="/signup" ><SignUpPage /></Route>
+          <Route exact path="/home" ><HomePage /></Route>
+          <Route exact path="/creator"><ShowCreatorDetailsPage /></Route>
+          <Route exact path="/manage"><CreatorManagePage /></Route>
+          <Route exact path="/saved"><BuyerSavedPaintsPage /></Route>
+          <Route exact path="/creation"><CreationDetailsPage /></Route>
+          <Route exact path="/newpaint"><CreatorNewPaintPage /></Route>
+          <Route exact path="/update-details"><CreatorDetailsPage /></Route>
+        </Switch>
+      </HashRouter>
+    </ActiveUserContext.Provider>
   );
 }
 
