0843452b93c2d0d8a72ab44d4d973957b7e9cf66
Zehavit <levi.zehavit@gmail.com>
2021-05-06 11:56:35 +0300

    connect welcome page to sign up page with prop is creator

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function SignUpPage(props) {
+    console.log(props.isCreator)
     return (
         <div>
            <h1>In SignUpPage</h1> 
+           {
+               props.isCreator === true ? <h2>is creator</h2> : <h2>is buyer</h2>
+           }
         </div>
     );
 }
--- src/pages/WellcomePage/WellcomePage.jsx
+++ src/pages/WellcomePage/WellcomePage.jsx
@@ -1,10 +1,27 @@
+
 import React from 'react';
+import { Col, Container, Row, Button } from 'react-bootstrap';
+import LoginOut from '../../components/LogInOut/LoginOut';
 
 function WellcomePage(props) {
+    
     return (
-        <div>
-            <h1>In WellcomePage</h1>
-        </div>
+        <Container>
+
+            <LoginOut></LoginOut>
+            <Row><h3>ברוכים הבאים ל paintIL!!!</h3></Row>
+            <Row><h2>האתר הישראלי לרכישת אומנות ישירות מהאומן, ללא דמי תיווך, בצורה נעימה וללא שטיקים </h2></Row>
+            <Row>השירות באתר ניתן לרשומים בלבד</Row>
+            <Row>
+                <h3>להרשמה עליך לבחור</h3>
+                <Col className="col-6">
+                    <Button href="#/signup" onClick={()=>props.setIsCreator(true)}>אומן</Button>
+                </Col>
+                <Col className="col-6">
+                    <Button href="#/signup" onClick={()=>props.setIsCreator(false)}>קונה</Button>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
