8fdd02db9428ead1ee32ca123e693754026ad160
Zehavit <levi.zehavit@gmail.com>
2021-05-09 22:00:11 +0300

    Wellcome page wprking and designed

--- src/pages/WellcomePage/WellcomePage.jsx
+++ src/pages/WellcomePage/WellcomePage.jsx
@@ -1,26 +1,29 @@
 
 import React from 'react';
 import { Col, Container, Row, Button } from 'react-bootstrap';
-import LogInOut from '../../components/LogInOut/LogInOut';
+import MyButton from '../../components/MyButton/MyButton';
 import './WellcomePage.css';
 
 function WellcomePage(props) {
-    
+    const signup = (isCreator => props.setIsCreator(isCreator));
     return (
+
         <Container className="p-wellcome">
-            <LogInOut/>
-            <Row><h3>ברוכים הבאים ל PaintIL!!!</h3></Row>
-            <Row><h2>האתר הישראלי לרכישת אומנות ישירות מהאומן, ללא דמי תיווך, בצורה נעימה וללא שטיקים </h2></Row>
-            <Row>*השירות באתר ניתן לרשומים בלבד</Row>
-            <Row>
-                <h3>להרשמה עליך לבחור</h3>
-                <Col className="col-6">
-                    <Button href="#/signup" onClick={()=>props.setIsCreator(true)}>אומן</Button>
-                </Col>
-                <Col className="col-6">
-                    <Button href="#/signup" onClick={()=>props.setIsCreator(false)}>קונה</Button>
-                </Col>
-            </Row>
+
+            <div className="p-text-content">
+                <MyButton href="/login" text="כניסה" />
+                <p><h2>ברוכים הבאים לאתר</h2></p>
+                <p><h1 className="eng">PaintsIL</h1></p>
+                <p><h2>האתר הישראלי לרכישת אומנות ישירות מהאומן, ללא דמי תיווך! </h2></p>
+                <p><h1>בצורה נעימה וללא שטיקים</h1></p>
+                <p><h4>*השירות באתר ניתן לרשומים בלבד</h4></p>
+                <h2>להרשמה עליך לבחור</h2>
+                <div className="signup-buttons">
+                    <MyButton href="/signup" onClick={signup} isCreator={true} text="אומן" />
+                    <MyButton href="/signup" onClick={signup} isCreator={false} text="קונה" />
+                </div>
+            </div>
+
         </Container>
     );
 }
