0e35a220a8792f3e5839f3df7fe7d69e7277bbb9
Zehavit <levi.zehavit@gmail.com>
2021-05-09 22:01:11 +0300

    login page working but nut desigen

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,34 +3,27 @@ import Parse from 'parse';
 import { Alert, Button, Form } from 'react-bootstrap';
 import { Link, Redirect } from 'react-router-dom';
 import ActiveUserContext from '../../shared/ActiveUserContext';
-import CreatorModel from '../../models/CreatorModel';
-import BuyerModel from '../../models/BuyerModel';
+import UserModel from '../../models/UserModel';
 
-function LoginPage({onLogin}) {
+function LoginPage({ onLogin }) {
     const [email, setEmail] = useState("");
     const [pwd, setPwd] = useState("");
     const [showInvalidLogin, setShowInvalidLogin] = useState(false);
     const activeUser = useContext(ActiveUserContext);
     if (activeUser) {
-        return <Redirect to="/home"/>
+        return <Redirect to="/home" />
     }
-    function login(e) {
+    async function login(e) {
         e.preventDefault();
-        let activeUser = null;
         // Pass the username and password to logIn function
-        Parse.User.logIn(email, pwd).then((parseUser) => {
-          if(parseUser.get("isCreator")) {
-                activeUser = new CreatorModel(parseUser);
-                console.log((activeUser.type));
-          }
-          else{
-            activeUser = new BuyerModel(parseUser);
-          }
-           onLogin(activeUser);
-        }).catch(error => {
+        // UserModel.login(email,pwd);
+        try {
+            const activeUser = await UserModel.login(email, pwd);
+            onLogin(activeUser);
+        } catch (error) {
             console.error('Error while logging in user', error);
             setShowInvalidLogin(true);
-        })
+        }
     }
     return (
         <div className="p-login">
@@ -40,15 +33,15 @@ function LoginPage({onLogin}) {
             <Form onSubmit={login}>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" 
+                    <Form.Control type="email" placeholder="Enter email"
                         value={email} onChange={e => setEmail(e.target.value)} />
                     <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
+                        We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" 
+                    <Form.Control type="password" placeholder="Password"
                         value={pwd} onChange={e => setPwd(e.target.value)} />
                 </Form.Group>
                 <Button variant="success" type="submit" block>
