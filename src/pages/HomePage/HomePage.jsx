ec4ce9cac17ef9a62304edb4e785c78ae600b235
Zehavit <levi.zehavit@gmail.com>
2021-05-09 22:01:52 +0300

    get paints to show work by user type

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ActiveUserContext from '../../shared/ActiveUserContext';
+import Parse from 'parse';
+import UserModel from '../../models/UserModel';
 
 function HomePage(props) {
+    const [paints, setPaints] = useState([]);
     const activeUser = useContext(ActiveUserContext);
+    
+    useEffect(() => {
+        async function fetchData() {
+            const PaintsToShow = await activeUser.getPaints();
+            setPaints(PaintsToShow);
+        }
+        if (activeUser) {
+            fetchData();
+        }
+    }, [activeUser])
+    
     return (
 
         <div>
             <h1>In HomePage</h1>
-            {activeUser.type === "creator" ?
-            <h2>creator</h2> :
-            <h2> buyer</h2>
+            {activeUser && activeUser.isCreator ?
+                <>
+                <h2>creator</h2>
+                {paints.map(paint => <img src={paint.img} alt=""></img>)}
+                </> :
+                <>
+                <h2> buyer</h2>
+                {paints.map(paint => <img src={paint.img} alt=""></img>)}
+                </>
             }
         </div>
     );
