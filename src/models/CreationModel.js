6f8c70095a23da24b83c90c34e7db3d542ff1a36
Zehavit <levi.zehavit@gmail.com>
2021-05-12 09:15:02 +0300

    fix get url from undefined

--- src/models/CreationModel.js
+++ src/models/CreationModel.js
@@ -3,8 +3,8 @@ export default class CreationModel {
         this.id = result.id;
         this.name = result.get("name");
         this.img = result.get("img").url();
-        if(result.get("additionalImg1") !== undefined) this.additionalImg1 = result.get("additionalImg1").url();
-        if(result.get("additionalImg2") !== undefined) this.additionalImg2 = result.get("additionalImg2").url();
+        if(result.get("additionalImg1")) this.additionalImg1 = result.get("additionalImg1").url();
+        if(result.get("additionalImg2")) this.additionalImg2 = result.get("additionalImg2").url();
         this.price = result.get("price");
         if(result.get("creationDate") !== undefined) this.creationDate = result.get("creationDate");
         this.width = result.get("width");
