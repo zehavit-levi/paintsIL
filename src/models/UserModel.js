03ef02e42d2594b0038515ce3e41bcb100ec382d
Zehavit <levi.zehavit@gmail.com>
2021-05-12 09:18:05 +0300

    add saved and watched to model

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -16,10 +16,13 @@ export default class UserModel {
         this.story = parseUser.get("story");
         this.occupation = parseUser.get("occupation");
         this.isCreator = parseUser.get("isCreator");
+        this.savedPaints = parseUser.get("savedPaints");
+        this.watchedPaints = parseUser.get("watchedPaints");
 
     }
 
     static activeUser = null;
+    static paints = null;
 
     static async login(email, pwd) {
         const parseUser = await Parse.User.logIn(email, pwd);
@@ -28,15 +31,40 @@ export default class UserModel {
     }
 
     async getPaints() {
-        console.log(UserModel.activeUser.id);
         const Paint = Parse.Object.extend('Paint');
         const query = new Parse.Query(Paint);
-        if(UserModel.activeUser.isCreator){
-        query.equalTo("creatorId", UserModel.activeUser.id);
+        if (UserModel.activeUser.isCreator) {
+            query.equalTo("creatorId", UserModel.activeUser.id);
         }
         const parsePaints = await query.find();
-        const paints = parsePaints.map(parsePaint => new CreationModel(parsePaint));
-        
-        return paints;
+        UserModel.paints = parsePaints.map(parsePaint => new CreationModel(parsePaint));
+        return UserModel.paints;
+    }
+
+    async getSavedPaints() {
+        console.log(UserModel.activeUser);
+        const Paint = Parse.Object.extend('Paint');
+        const query = new Parse.Query(Paint);
+        query.containedIn("objectId", UserModel.activeUser.savedPaints)
+        const parsePaints = await query.find();
+        UserModel.paints = parsePaints.map(parsePaint => new CreationModel(parsePaint));
+
+        return UserModel.paints;
+    }
+
+    async getFilterdPaints(filterText,filterBy){
+        console.log("filterText:", filterText, "filterBy", filterBy);
+        if(filterBy === "creationName"){
+            return UserModel.paints.filter((paint,index) => paint.name === filterText);
+        }
+        else if(filterBy !== "creationName" && filterText){
+            const User = Parse.Object.extend('User');
+            const query = new Parse.Query(User);
+            query.fullText(filterBy, filterText);
+            const parseUser = await query.find();
+            const results = UserModel.paints.filter(paint => paint.creatorId === parseUser.id);
+            return results;
+        }
+        return UserModel.paints;
     }
 }
