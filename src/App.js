85b12f2ce7888f5e12802446d1af78a7685aec35
Zehavit <levi.zehavit@gmail.com>
2021-05-13 10:38:35 +0300

    remove paints context

--- src/App.js
+++ src/App.js
@@ -12,30 +12,27 @@ import CreatorNewPaintPage from './pages/CreatorNewPaintPage/CreatorNewPaintPage
 import CreatorDetailsPage from './pages/CreatorDetailsPage/CreatorDetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ActiveUserContext from './shared/ActiveUserContext';
-import PaintsContext from './shared/PaintsContext';
 import { useState } from 'react';
 function App() {
   const [activeUser, setActiveUser] = useState();
   const [isCreator, setIsCreator] = useState(undefined);
-  const [paints, setPaints] = useState();
+  const [paints,setPaints] = useState([]);
   return (
     <ActiveUserContext.Provider value={activeUser} >
-      <PaintsContext.Provider value={paints}>
       <HashRouter>
         <Switch>
           {isCreator !== undefined? <Route exact path="/signup" ><SignUpPage isCreator={isCreator}/></Route> : null}
           <Route exact path="/" ><WellcomePage setIsCreator={setIsCreator}/></Route>
           <Route exact path="/login" component={LoginPage}><LoginPage onLogin={user => setActiveUser(user) } /></Route>
-          <Route exact path="/home" ><HomePage setPaints={setPaints}/></Route>
+          <Route exact path="/home" ><HomePage setPaints ={setPaints} paints={paints}/></Route>
           <Route exact path="/creator"><ShowCreatorDetailsPage /></Route>
           <Route exact path="/manage"><CreatorManagePage /></Route>
-          <Route exact path="/saved"><BuyerSavedPaintsPage setPaints={setPaints} /></Route>
+          <Route exact path="/saved"><BuyerSavedPaintsPage setPaints ={setPaints} paints={paints}/></Route>
           <Route exact path="/creation/:index"><CreationDetailsPage /></Route>
           <Route exact path="/newpaint"><CreatorNewPaintPage /></Route>
           <Route exact path="/update-details"><CreatorDetailsPage /></Route>
         </Switch>
       </HashRouter>
-      </PaintsContext.Provider>
     </ActiveUserContext.Provider>
   );
 }
