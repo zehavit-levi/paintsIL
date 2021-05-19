1362e13bd54dfe0413066a04c6e785db3b008376
Zehavit <levi.zehavit@gmail.com>
2021-05-19 22:40:14 +0300

    buyer nav bar generic

--- src/components/BuyerNavBar/BuyerNavBar.jsx
+++ src/components/BuyerNavBar/BuyerNavBar.jsx
@@ -8,14 +8,15 @@ function BuyerNavBar(props) {
 
 
     return (
-        <Navbar className="buyer-navbar" bg="light" expand="lg">
-            <Navbar.Brand href="/">PaintsIL</Navbar.Brand>
+        <Navbar className="buyer-navbar"  expand="lg">
+            <Navbar.Brand className="eng" href="/">PaintsIL</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto col-lg-6">
-                    <Nav.Link href="#home">עמוד הבית</Nav.Link>
-                    <Nav.Link href="#saved">התמונות ששמרתי</Nav.Link>
+                    <Nav.Link className="home" href="#home">עמוד הבית</Nav.Link>
+                    <Nav.Link className="saved" href="#saved">התמונות ששמרתי</Nav.Link>
                 </Nav>
+                {props.page === "home" ?
                 <Form className="form-filter col-lg-6">
                     <Row>
                         <div className="filter-container col-md-6 col-12">
@@ -25,13 +26,13 @@ function BuyerNavBar(props) {
                         </div>
                         <div className="select-container col-md-6 col-12">
                             <Form.Control value={props.filterBy} as="select" onChange={(e) => props.setFilterBy(e.target.value)}>
-                                <option value="username">שם האומן</option>
+                                <option value="userName">שם האומן</option>
                                 <option value="creationName">שם היצירה</option>
-                                <option value="size">גודל</option>
+                                {/* <option value="size">גודל</option> */}
                             </Form.Control>
                         </div>
                     </Row>
-                </Form>
+                </Form> : null}
             </Navbar.Collapse>
         </Navbar>
     );
