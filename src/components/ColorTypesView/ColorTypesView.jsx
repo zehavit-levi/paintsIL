ff864043d916f66920a639f8210ae26fbe7103ea
Zehavit <levi.zehavit@gmail.com>
2021-05-20 22:38:58 +0300

    change defualt icons in carusel

--- src/components/ColorTypesView/ColorTypesView.jsx
+++ src/components/ColorTypesView/ColorTypesView.jsx
@@ -1,27 +1,36 @@
 import { Carousel, Container } from 'react-bootstrap';
+import './ColorTypesView.css';
 
 function ColorTypesView(props) {
-    
-    const items = props.types.map((colorType,index) => {
-        return(
-        <Carousel.Item key= {index}>
-                    <img 
-                        style={{width:"50%", opacity:"0.7", border: "2px inset"}}
-                        src={colorType.img}
-                        alt="First slide"
 
-                    />
-                    <Carousel.Caption style={{position:"relative"}}>
-                        <h4 style={{color:"black",position:"absolute", bottom:"20"}}>{colorType.name}</h4>
-                    </Carousel.Caption>
-                </Carousel.Item>
+    const items = props.types.map((colorType, index) => {
+        return (
+            <Carousel.Item key={index}>
+                <img className="carouselImg"
+                    src={colorType.img}
+                    alt="First slide"
+
+                />
+                <Carousel.Caption style={{ position: "relative" }}>
+                    <h4>{colorType.name}</h4>
+                </Carousel.Caption>
+            </Carousel.Item>
         )
     })
     return (
         <Container className="carouselContainer">
-            <Carousel fade>
-                {items ?  items : null}
-            </Carousel>
+            {props.types.length > 1 ?
+                <Carousel fade prevLabel={null} nextLabel={null}>
+                    {items ? items : null}
+                </Carousel>
+                :
+                <>
+                    <img className="carouselImg"
+                        src={props.types[0].img}
+                        alt="First slide"
+                    />
+                    <h4>{props.types[0].name}</h4>
+                </>}
         </Container>
     );
 }
