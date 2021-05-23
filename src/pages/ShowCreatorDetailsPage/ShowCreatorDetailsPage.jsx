d555cde2a65089596082e74e0716a61f4669b4c3
Zehavit <levi.zehavit@gmail.com>
2021-05-23 22:26:40 +0300

    ShowCreatorDetailsPage start of build

--- src/pages/ShowCreatorDetailsPage/ShowCreatorDetailsPage.jsx
+++ src/pages/ShowCreatorDetailsPage/ShowCreatorDetailsPage.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import { useParams } from 'react-router';
+import BuyerNavBar from '../../components/BuyerNavBar/BuyerNavBar';
+import UserModel from '../../models/UserModel';
+import './ShowCreatorDetailsPage.css'
 
-function ShowCreatorDetailsPage(props) {
+function ShowCreatorDetailsPage({ onLogout }) {
+    const { index } = useParams();
+    const [creator, setCreator] = useState(null);
+    useEffect(() => {
+        async function fetchData() {
+            const creator = await UserModel.getCreator(index);
+            console.log(creator);
+            setCreator(creator);
+        }
+        if (index) {
+            fetchData();
+        }
+    }, [index])
+
     return (
-        <div>
-            <h1>In ShowCreatorDetailsPage</h1>
-        </div>
+        <Container className="p-show-creator-details">
+            <BuyerNavBar onLogout={onLogout} />
+            {creator ?
+                <>
+                    <h1 className="title">{creator.userName}</h1>
+                    {creator.story? <Row className="story-container"><div className="story">{creator.story}</div></Row>:null}
+
+                </>
+                :
+                null
+            }
+        </Container>
     );
 }
 
